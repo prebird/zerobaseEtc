8e3ab68a8bf9dd256bf27834aeb448e7fa45eab9
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-05 15:42:08 +0900

    Initial commit

---
+++ src/main/java/com/example/zerobaseetc/ZerobaseEtcApplication.java
@@ -0,0 +1,13 @@
+package com.example.zerobaseetc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerobaseEtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerobaseEtcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zerobaseetc/ZerobaseEtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zerobaseetc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerobaseEtcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
